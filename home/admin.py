885a55cc2bddf7dcb22a70687aa0d98aed2a3dea
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-05 14:05:23 +0300

    family field read-only

--- home/admin.py
+++ home/admin.py
@@ -18,7 +18,8 @@ def generate_unique_username_from_str(name):
     return username
 
 class MemberAdmin(admin.ModelAdmin):
-    exclude = ('user', 'family')
+    exclude = ('user',)
+    readonly_fields = ('family',)
     def save_model(self, request, obj, form, change):
         if not obj.user:  # if the teacher is being created for the first time
             # Create a new user
