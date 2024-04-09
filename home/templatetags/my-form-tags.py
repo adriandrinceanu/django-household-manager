b16b29ebb582025aae1f8f8f34071285fdb87038
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-09 15:45:39 +0300

    updates

--- home/templatetags/my-form-tags.py
+++ home/templatetags/my-form-tags.py
@@ -1,10 +1,15 @@
 # myapp/templatetags/add_budget_tag.py
 from django import template
-from home.forms import BudgetCreationForm
+from home.forms import MonthlyBudgetCreationForm, BudgetCreationForm
 
 register = template.Library()
 
+@register.inclusion_tag('pages/add_monthly_budget.html', name='monthly_budget_form')
+def monthly_budget_form(username):
+    form = MonthlyBudgetCreationForm()
+    return {'form': form, 'username': username}
+
 @register.inclusion_tag('pages/add_budget.html', name='budget_form')
-def budget_form():
+def budget_form(username):
     form = BudgetCreationForm()
-    return {'form': form}
+    return {'form': form, 'username': username}
