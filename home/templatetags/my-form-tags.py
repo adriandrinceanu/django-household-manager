72dfd8f0cba39584bf430ed210791dbacdb25a74
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-15 13:24:38 +0300

    added admin group checker

--- home/templatetags/my-form-tags.py
+++ home/templatetags/my-form-tags.py
@@ -23,3 +23,7 @@ def expense_form(username):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name="in_group")
+def in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
