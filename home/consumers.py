332ada8cb3e7909fe97206fb3a5bd4309fa3b866
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-18 15:35:12 +0300

    add chat family room

--- home/consumers.py
+++ home/consumers.py
@@ -5,6 +5,7 @@ from asgiref.sync import async_to_sync
 from channels.db import database_sync_to_async
 import json
 import logging
+from datetime import datetime
 from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
@@ -34,12 +35,12 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
         return self.user.member.family.id
 
     async def disconnect(self, close_code):
-        logger.info(f"Disconnecting from group {self.group_name}")
         # Leave the group
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
+        logger.info(f"Disconnecting from group {self.group_name}")
         
     @database_sync_to_async
     def get_notifications(self):
@@ -94,7 +95,9 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
     async def send_notification(self, event):
         notification = event['notification']
         # Send message to WebSocket
-        await self.send_json(notification)
+        await self.send_json({
+        'notification': notification,
+    })
 
     
 
@@ -102,3 +105,118 @@ class NotificationConsumer(AsyncJsonWebsocketConsumer):
     # async def send_notification(self, event):
     #     logger.info(f"Sending notification: {event['text']}")
     #     await self.send_json(event["text"])
+    
+    
+class FamilyChatConsumer(AsyncWebsocketConsumer):
+    @sync_to_async
+    def get_username(self):
+        # Check if user information is already stored in the connection object
+        if hasattr(self, 'user'):
+            return self.user.username
+
+        # If not stored, retrieve it from the database using sync_to_async
+        from django.contrib.auth.models import User
+        user = User.objects.get(pk=self.scope['user'].pk)
+        return user.username
+    
+    async def connect(self):
+        from .models import Message 
+        self.room_name = self.scope['url_route']['kwargs']['family_name']
+        self.room_group_name = f"chat_{self.room_name}"
+        logger.info(f"Room group: {self.room_group_name}")
+        # Accept the WebSocket connection
+        await self.accept()
+        
+        # Load past messages from the database
+        past_messages = await sync_to_async(Message.objects.filter)(chat_id=self.room_name)
+        past_messages = await sync_to_async(list)(past_messages.order_by('timestamp'))
+        
+
+        
+        for message in past_messages:
+            # username = await self.get_username() #old username retrieval. it returned the current username of the logged user.
+            username = message.username   # Get the username from the message
+            timestamp = str(naturaltime(message.timestamp))  #Convert the timestamp to natural time
+            await self.send(text_data=json.dumps({
+                'message': message.content,
+                'username': username,
+                'timestamp': timestamp
+            }))
+        logger.info(f"Connected to {self.room_name}")
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        logger.info(f"Joined room group: {self.room_group_name}")
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        logger.info(f"Left room group: {self.room_group_name}")
+    
+        
+        
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']  #Extract the username
+        
+        # Save the message to the database
+        await self.save_message(username, message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+        logger.info(f"Received message: {message}")
+        
+    async def chat_message(self, event):
+        # This method is called whenever a 'chat_message' is received
+        message = event['message']
+        # username = self.scope["user"].username # for selfkeeping
+        username = event['username']
+        now = datetime.now()
+        
+         # Humanize the timestamp
+        timestamp = str(naturaltime(now))
+
+        # Send the message to the WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'timestamp': timestamp
+        }))
+        logger.info(f"Sending message: {message}")
+
+        
+        
+    @sync_to_async
+    def save_message(self, username, message):
+        from .models import Message, Member
+
+        try:
+            # Get the member object using the username
+            member = Member.objects.get(user__username=username)
+
+            if member:
+                # Access the user and family associated with the member
+                self.user = self.scope["user"]
+                family = member.family  # Get the family
+                username = self.user.username
+                # Save the message with the user and family
+                Message.objects.create(user=self.user, family=family, username=username, content=message, chat_id=self.room_name)
+            else:
+                print("Error: Member not found for username", username)
+        except Member.DoesNotExist:
+            print("Error: Member not found for username", username)
