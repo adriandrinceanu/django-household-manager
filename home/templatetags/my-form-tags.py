230749e4213f62bd59762ab8bfc942278bbcd312
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-22 20:22:14 +0300

    not sure if ok, but it works

--- home/templatetags/my-form-tags.py
+++ home/templatetags/my-form-tags.py
@@ -1,8 +1,10 @@
 # myapp/templatetags/add_budget_tag.py
 from django import template
-from home.forms import MonthlyBudgetCreationForm, BudgetCreationForm, ExpenseCreationForm
-from home.models import Member
+from home.forms import MonthlyBudgetCreationForm, BudgetCreationForm, ExpenseCreationForm, MonthlyBudget
+from home.models import Member, Budget
 from django.contrib.auth.models import User
+from django.db.models import Sum
+
 
 
 register = template.Library()
@@ -18,7 +20,13 @@ def budget_form(username):
     member = Member.objects.get(user=user)
     family = member.family
     form = BudgetCreationForm(family=family)
-    return {'form': form, 'username': username}
+    # Get the total budget for the month
+    total_monthly_budget = MonthlyBudget.objects.filter(family=family).aggregate(Sum('amount'))['amount__sum'] or 0
+    # Get the total allocated budget for the month
+    total_allocated_budget = Budget.objects.filter(monthly_budget__family=family).aggregate(Sum('amount'))['amount__sum'] or 0
+    # Calculate the remaining budget
+    remaining_budget = total_monthly_budget - total_allocated_budget
+    return {'form': form, 'username': username, 'remaining_budget': remaining_budget}
 
 @register.inclusion_tag('pages/add_expense.html', name='expense_form')
 def expense_form(username):
