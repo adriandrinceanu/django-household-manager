dd9fe4a215cbc5e9e25d9c34b8b6d356bb3c86ce
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-23 11:10:19 +0300

    switched to wss (Secure websocket) for deploy

--- home/routing.py
+++ home/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     # re_path(r'wss/chat/(?P<username>\w+)_(?P<username2>\w+)_chatroom/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
-    re_path(r'ws/chat/(?P<family_name>\w+)_chatroom(?P<family_id>\w+)/$', consumers.FamilyChatConsumer.as_asgi()),
+    re_path(r'wss/notifications/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<family_name>\w+)_chatroom(?P<family_id>\w+)/$', consumers.FamilyChatConsumer.as_asgi()),
 ]
